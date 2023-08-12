b473a75e88ad664f441f61f8089419015b1b14e6
hzsyahzin <hzsyahzin@gmail.com>
2023-08-12 23:31:03 +0100

    Added FAP and MOF support

--- airrollfinder.c
+++ airrollfinder.c
@@ -1,57 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
+#include <math.h>
 
 #include "armordata.h"
 
-void get_airrolls(int endurance)
+
+void get_airrolls(int endurance, bool fap)
 {
-	char str[100];
-	sprintf(str, "%dend.txt", endurance);
-
 	float max_weight = (float)endurance + 40.0f;
+	int results = 0;
 
-	float lb = max_weight * 0.25f;
-	float ub = max_weight * 0.25000088f;
+	char str[100];
+	if (fap)
+	{
+		sprintf(str, "%dEND-FAP.txt", endurance);
+		max_weight *= 1.2f;
+	}
+	else
+		sprintf(str, "%dEND.txt", endurance);
 
 	FILE *fptr = fopen(str, "w");
 
-	int results = 0;
+	float target, boundary, compared_weight;
+	bool masked;
+
 	for (int h=0; h<70; h++)
 	{
+		if (!strcmp("Mask of the Father", head_names[h]))
+		{
+			masked = true;
+			compared_weight = max_weight * 1.05f;
+		}
+		else
+		{
+			masked = false;
+			compared_weight = max_weight;
+		}
+
+		target = compared_weight * 0.25f;
+		boundary = compared_weight * 0.25000088f;
+
 		for (int c=0; c<59; c++)
 		{
 			for (int g=0; g<56; g++)
 			{
 				for (int l=0; l<59; l++)
 				{
-					float raw_total = head[h] + chest[c] + gloves[g] + legs[l];
-					if (raw_total > ub)
+					float sum = head[h] + chest[c] + gloves[g] + legs[l];
+					float balance = target - sum;
+
+					if (balance < 0.0f)
 						continue;
 
-					float fractional = raw_total - (int)raw_total;
-					if (fractional <= 0.0f || fractional >= 0.1f)
+					float fractional = round((balance - floor(balance)) * 10) / 10;
+					if (fractional != 0.0f && fractional != 0.5f)
 						continue;
 
-					int scaled_diff = ((int)(lb * 10) - (int)(raw_total * 10));
-					if (scaled_diff % 5 != 0)
+					balance = floor(balance) + fractional;
+
+					sum = balance + head[h] + chest[c] + gloves[g] + legs[l];
+
+					if (sum <= target || sum >= boundary)
 						continue;
 
-					float balance = scaled_diff / 10.0f;
+					fprintf(fptr, "%.1f + %.1f + %.1f + %.1f (+%.1f)\t", 
+						head[h], chest[c], gloves[g], legs[l], balance);
+					fprintf(fptr, "%s + %s + %s + %s\n", head_names[h], 
+						chest_names[c], gloves_names[g], legs_names[l]);
 
-					// NOTE: using raw_total from earlier breaks this for some reason
-					float total = balance
-						+ head[h] + chest[c] + gloves[g] + legs[l];
-
-					if (total > lb && total < ub)
-					{
-						fprintf(fptr, "%.1f + %.1f + %.1f + %.1f (+%.1f)\t", 
-							head[h], chest[c], gloves[g], legs[l], balance);
-						fprintf(fptr, "%s + %s + %s + %s\n", head_names[h], 
-							chest_names[c], gloves_names[g], legs_names[l]);
-
-						results += 1;
-					}
+					++results;
 				}
 			}
 		}
@@ -64,12 +83,13 @@ void get_airrolls(int endurance)
 
 int main(int argc, char* argv[])
 {
-	if (argc != 2)
+	if (argc != 3)
 	{
-		printf("airoll-finder requires 1 argument (endurance)\n");
+		printf("airoll-finder requires 2 arguments: [ENDURANCE] [FAP (y/n)]\n");
 		return -1;
 	}
 
-	get_airrolls(strtol(argv[1], NULL, 10));
+	bool fap = (!strcmp(argv[2], "y") || !strcmp(argv[2], "Y"));
+	get_airrolls(strtol(argv[1], NULL, 10), fap);
 	return 0;
 }
